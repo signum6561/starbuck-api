c986e12a63eb8f423aa487225109b136d40943bd
Deus Ex Machina <quiocdat777@gmail.com>
2024-06-17 12:57:58 +0700

    implements JWT Auth

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -6,16 +6,16 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-
     public function register(Request $request)
     {
         try {
-            $validateUser = Validator::make(
+            $validator = Validator::make(
                 $request->all(),
                 [
                     'username' => 'required|min:2|unique:users,username',
@@ -24,27 +24,24 @@ class AuthController extends Controller
                 ]
             );
 
-            if ($validateUser->fails()) {
+            if ($validator->fails()) {
                 return response()->json([
-                    'status' => 'error',
                     'message' => 'Validation error',
-                    'errors' => $validateUser->errors()
+                    'errors' => $validator->errors()
                 ], 401);
             }
-            $user = User::create([
-                'username' => $request->username,
-                'email' => $request->email,
-                'password' => Hash::make($request->password)
-            ]);
+
+            $user = User::create(array_merge(
+                $validator->validated(),
+                ['password' => bcrypt($request->password)]
+            ));
 
             return response()->json([
-                'status' => 'success',
-                'message' => 'User Created Successfully',
-                'token' => $user->createToken("API_TOKEN")->plainTextToken
+                'message' => 'User Register Successfully',
+                'user' => $user
             ], 200);
         } catch (\Throwable $th) {
             return response()->json([
-                'status' => 'error',
                 'message' => $th->getMessage()
             ], 500);
         }
@@ -58,7 +55,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         try {
-            $validateUser = Validator::make(
+            $validator = Validator::make(
                 $request->all(),
                 [
                     'email' => 'required|email',
@@ -66,49 +63,100 @@ class AuthController extends Controller
                 ]
             );
 
-            if ($validateUser->fails()) {
+            if ($validator->fails()) {
                 return response()->json([
-                    'status' => 'error',
                     'message' => 'Validation error',
-                    'errors' => $validateUser->errors()
+                    'errors' => $validator->errors()
                 ], 401);
             }
 
             if (!Auth::attempt($request->only(['email', 'password']))) {
                 return response()->json([
-                    'status' => 'error',
                     'message' => 'Invalid email or password',
                 ], 401);
             }
 
-            $user = User::where('email', $request->email)->first();
 
-            return response()->json([
-                'status' => 'success',
-                'message' => 'User Logged In Successfully',
-                'token' => $user->createToken("API_TOKEN")->plainTextToken
-            ], 200);
+            if (!$token = auth()->attempt($validator->validated())) {
+                return response()->json(['message' => 'Unauthenticated'], 401);
+            }
+
+            $refreshToken = $this->createRefreshToken();
+            return $this->respondWithToken($token, $refreshToken);
         } catch (\Throwable $th) {
             return response()->json([
-                'status' => 'error',
                 'message' => $th->getMessage()
             ], 500);
         }
     }
 
-    public function logout(Request $request)
+    public function logout()
     {
-        $request->user()->currentAccessToken()->delete();
-        return response()->json([
-            'message' => 'User successfully logout',
-        ], 200);
+        try {
+            if (!auth()->user()) {
+                return response()->json(['message' => 'Unauthenticated'], 401);
+            }
+            JWTAuth::invalidate(JWTAuth::getToken());
+            return response()->json([
+                'message' => 'User successfully logout',
+            ], 200);
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
-    public function getUser(Request $request)
+    public function getUser()
+    {
+        $user = auth()->user();
+        if (!$user) {
+            return response()->json(['message' => 'Unauthenticated'], 401);
+        }
+        return response()->json(['message' => 'Get user data successfully', 'data' => $user]);
+    }
+
+    public function refresh()
+    {
+        try {
+            $refreshToken = request()->refresh_token;
+            $decoded = JWTAuth::getJWTProvider()->decode($refreshToken);
+            if (time() >= $decoded['exp']) {
+                return response()->json(['message' => 'Refresh Token expired'], 401);
+            }
+            $user = User::find($decoded['sub']);
+
+            if ($user->refresh_token != $decoded['jti']) {
+                return response()->json(['message' => 'Invalid Refresh Token'], 404);
+            }
+
+            $newRefreshToken = $this->createRefreshToken();
+            JWTAuth::invalidate(JWTAuth::getToken());
+            $newToken = auth()->login($user);
+            return $this->respondWithToken($newToken, $newRefreshToken);
+        } catch (JWTException $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    private function respondWithToken($token, $refreshToken)
     {
         return response()->json([
-            'status' => 'success',
-            'data' => $request->user()
-        ], 200);
+            'access_token' => $token,
+            'refresh_token' => $refreshToken,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ]);
+    }
+
+    private function createRefreshToken()
+    {
+        $data = [
+            'sub' => auth()->user()->id,
+            'jti' => rand() . time(),
+            'exp' => time() + config('jwt.refresh_ttl')
+        ];
+        auth()->user()->update(['refresh_token' => $data['jti']]);
+        return JWTAuth::getJWTProvider()->encode($data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,20 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\CustomerController;
 use App\Http\Controllers\Api\V1\InvoiceController;
 
-Route::get('/auth/user', [AuthController::class, 'getUser'])->middleware('auth:sanctum');
-Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/login', [AuthController::class, 'login']);
-Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
+Route::group(['prefix' => 'auth', 'middleware' => 'api'], function () {
+    Route::get('user', [AuthController::class, 'getUser']);
+    Route::post('register', [AuthController::class, 'register'])->withoutMiddleware('api');
+    Route::post('login', [AuthController::class, 'login'])->withoutMiddleware('api');
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+});
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'api'], function () {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
 });
